5ecbba37c7c6c4d1b3d4c7158b68cc5231ae050c
Andrq008 <99258356+Andrq008@users.noreply.github.com>
2023-11-28 12:21:18 +0300

    Update bot_parse_users.py
    other

--- bot_parse_users.py
+++ bot_parse_users.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 
-api_id = 14576105
-api_hash = 'a8e164ef42511799656592b2bf96d87d'
+api_id = 1...
+api_hash = ''
 app_version="Android 8.7.4"
 device_model="LM-Q720"
 system_version="Android"
